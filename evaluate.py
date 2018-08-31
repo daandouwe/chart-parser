41002a3a952efa87a334c440ef7747b2a1f86af0
daandouwe <daan_douwe@outlook.com>
2018-08-31 14:34:37 +0200

    Using EVALB for evaluation.

--- evaluate.py
+++ evaluate.py
@@ -4,13 +4,15 @@ import subprocess
 from PYEVALB import scorer, parser
 
 
-def evalb_python(pred_path, gold_path, result_path):
+def pyevalb(pred_path, gold_path, result_path):
+    """Use PYEVALB to score trees."""
     scorer.Scorer().evalb(gold_path, pred_path, result_path)
 
 
 def evalb(evalb_dir, pred_path, gold_path, result_path, ignore_error=1000):
+    """Use original EVALB to score trees."""
     evalb_dir = os.path.expanduser(evalb_dir)
-    assert os.path.exists(evalb_dir)
+    assert os.path.exists(evalb_dir), f'Do you have EVALB installed at {evalb_dir}?'
     evalb_exec = os.path.join(evalb_dir, "evalb")
     command = '{} {} {} -e {} > {}'.format(
         evalb_exec,
