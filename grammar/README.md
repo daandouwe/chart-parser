6e18a316d98d92ca29c0e0ae21431e648833b5e2
daandouwe <daan_douwe@outlook.com>
2018-09-03 17:30:39 +0200

    Changed data script.

--- grammar/README.md
+++ grammar/README.md
@@ -8,6 +8,7 @@ To obtain a grammar, run `./get-grammar.sh`. The script does the following:
   * Replace all numbers with `<num>`.
   * Replace with `<unk>` everything beyond a certain threshold (default vocab size is 10k).
 * Obtain a binarized grammar in CNF from the training set in `lopar` format using `treetools`.
+* Two types of grammars are extracted: a vanilla CNF grammar, and a v1:h1 Markovized CNF grammar (see [Klein and Manning 2003](http://delivery.acm.org/10.1145/1080000/1075150/p423-klein.pdf?ip=89.99.242.224&id=1075150&acc=OPEN&key=4D4702B0C3E38B35%2E4D4702B0C3E38B35%2E4D4702B0C3E38B35%2E6D218144511F3437&__acm__=1535987878_fdb08a617541f60b68baf3aff0d6ad99)).
 * Rewrite the `lopar` format to a custom format.
 
 The final grammar is stored in `grammar/train/train.grammar` and has the following format:
--- grammar/get-grammar.sh
+++ grammar/get-grammar.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 VOCAB_SIZE=10000
-MARKOV=false
 
-set -x  # echo on
 
 mkdir -p data train dev test
 
 # Get trees from the benepar if needed.
 if [[ ! -e data/train.trees ]]; then
-    echo Downloading data.
+    echo 'Downloading tree data.'
     wget https://raw.githubusercontent.com/nikitakit/self-attentive-parser/master/data/02-21.10way.clean \
         -P data/train.trees
     wget https://raw.githubusercontent.com/nikitakit/self-attentive-parser/master/data/22.auto.clean \
@@ -17,10 +15,11 @@ if [[ ! -e data/train.trees ]]; then
         -P data/test.trees
 fi
 
+
 # Get EVALB if needed.
 if [[ ! -d ../EVALB ]]; then
-    echo Downloading EVALB.
-    cd ..  # Install in main directory.
+    echo 'Downloading EVALB.'
+    cd ..  # Install EVALB in main directory.
     wget https://nlp.cs.nyu.edu/evalb/EVALB.tgz
     tar -xzf EVALB.tgz
     rm EVALB.tgz
@@ -30,19 +29,22 @@ if [[ ! -d ../EVALB ]]; then
     cd ../grammar  # Return to the folder grammar for the rest of the processing.
   fi
 
-# Get Treetools if needed.
+
+# Get treetools if needed.
 if [[ ! -d treetools ]]; then
-  echo Downloading treetools.
+  echo 'Downloading treetools.'
     git clone https://github.com/wmaier/treetools.git
     cd treetools
+    sed -i -e \
+        's/print(substitute_terminals.terminals)/# print(substitute_terminals.terminals)/g' \
+        trees/transform.py  # Do not print all the substitutions to terminal!
     python setup.py install --user
     cd ..
 fi
 
 
-echo Processing trees into grammar.
-
 # Get terminals from train, dev and test sets.
+echo 'Exracting tokens from trees.'
 treetools/treetools transform data/train.trees train.tokens \
     --src-format brackets --dest-format terminals
 treetools/treetools transform data/dev.trees dev.tokens \
@@ -50,39 +52,43 @@ treetools/treetools transform data/dev.trees dev.tokens \
 treetools/treetools transform data/test.trees test.tokens \
     --src-format brackets --dest-format terminals
 
+
 # For preprocessing of train-set: gather substitutions (lowercase, <unk>, and <num>).
 python get-lex-sub.py train.tokens $VOCAB_SIZE > train.subs
 
+
 # Let treetools apply the substitutions to the train set.
+echo 'Pre-processing tokens of trees.'
 treetools/treetools transform data/train.trees train.processed \
     --trans substitute_terminals --params terminalfile:train.subs \
     --src-format brackets --dest-format brackets
 
+
 # Treetools messes with the spacing...
 ./add-space.py train.processed
 
-# Use nltk to convert trees to CNF. Optional Markovivization.
-if [ $MARKOV = true ]; then
-    echo Markovize the grammar: v1:h1
-    ./nltk-cnf.py train/train.processed train/train.markov.processed --markov 1:1 --collapse-unaries
 
-    NAME=train.markov
-else
-    ./nltk-cnf.py train/train.processed train/train.processed --collapse-unaries
+# Use nltk to convert trees to two types of CNF: vanilla and markovized.
+echo 'Making vanilla CNF trees.'
+./make-cnf.py train.processed train/train.vanilla.processed --collapse-unaries
 
-    NAME=train
-fi
+echo 'Making Markov CNF trees (v1:h1).'
+./make-cnf.py train.processed train/train.markov.processed --collapse-unaries --markov 1:1
 
-# Get rules from grammar
-treetools/treetools grammar train/$NAME.processed train/$NAME \
-    leftright --src-format brackets --dest-format lopar
+for name in train.vanilla train.markov; do
+    echo 'Extracting grammar from' $name
 
-# Process the grammar output to our own format.
-./custom-format.py train/$NAME.gram train/$NAME.lex > train/$NAME.grammar
+    # Get rules from grammar
+    treetools/treetools grammar train/$name.processed train/$name \
+        leftright --src-format brackets --dest-format lopar
+
+    # Process the grammar output to our own format.
+    ./custom-format.py train/$name.gram train/$name.lex > train/$name.grammar
+done
 
 
 # Cleanup.
 rm train.subs
-mv $NAME.processed train.tokens vocab.json train
+mv train.processed train.tokens vocab.json train
 mv dev.tokens dev
 mv test.tokens test
