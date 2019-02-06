72a6d029a4fdb7666d7bd82cf5d5595a7acf8687
daandouwe <daan_douwe@outlook.com>
2019-02-06 11:39:54 +0100

    Compute perplexity with inside.

--- cky/_cky.pyx
+++ cky/_cky.pyx
@@ -1,24 +1,25 @@
-#cython: language_level=3, boundscheck=False, wraparound=False, profile=True
+#cython: language_level=3, boundscheck=False, wraparound=False
 import cython
 import numpy as np
 cimport numpy as np
-from libc.math cimport log
+from libc.math cimport log, exp
+from numpy.math cimport INFINITY
 
 
 def cky(
-    int[:] sentence,
-    int sent_len,
-    float[:,:,:] score,
-    int[:,:,:,:] back,
-    int num_lex_rules,
-    int num_unary_rules,
-    int num_binary_rules,
-    int[:,:] lex_rules,
-    int[:,:] unary_rules,
-    int[:,:] binary_rules,
-    float[:] lex_prob,
-    float[:] unary_prob,
-    float[:] binary_prob
+        int[:] sentence,
+        int sent_len,
+        float[:,:,:] score,
+        int[:,:,:,:] back,
+        int num_lex_rules,
+        int num_unary_rules,
+        int num_binary_rules,
+        int[:,:] lex_rules,
+        int[:,:] unary_rules,
+        int[:,:] binary_rules,
+        float[:] lex_prob,
+        float[:] unary_prob,
+        float[:] binary_prob
     ):
 
     cdef int i, j
@@ -42,7 +43,7 @@ def cky(
             end = begin + span
             for split in range(begin + 1, end):
                 for i in range(num_binary_rules):
-                    A, B, C = binary_rules[i][0], binary_rules[i][1], binary_rules[i][2]
+                    A, B, C = binary_rules[i][0], binary_rules[i][1], binary_rules[i][2]  # A -> B C
                     rule_prob = binary_prob[i]
                     prob = score[B][begin][split] + score[C][split][end] + log(rule_prob)
                     if prob > score[A][begin][end]:
@@ -52,3 +53,63 @@ def cky(
                         back[A][begin][end][2] = C
 
     return score, back
+
+
+def inside(
+        int[:] sentence,
+        int sent_len,
+        float[:,:,:] score,
+        int num_lex_rules,
+        int num_unary_rules,
+        int num_binary_rules,
+        int num_nonterminals,
+        int[:,:] lex_rules,
+        int[:,:] unary_rules,
+        int[:,:] binary_rules,
+        float[:] lex_prob,
+        float[:] unary_prob,
+        float[:] binary_prob
+    ):
+
+    cdef int i, j
+    cdef int span, begin, end, split
+    cdef int A, B, C, w
+    cdef float prob, rule_prob
+
+    for i in range(sent_len):
+        for j in range(num_lex_rules):
+            A, w = lex_rules[j][0], lex_rules[j][1]  # A -> w
+            if w == sentence[i]:
+                score[A][i][i+1] = log(lex_prob[j])
+
+    for span in range(2, sent_len + 1):
+        for begin in range(0, sent_len - span + 1):
+            end = begin + span
+            for split in range(begin + 1, end):
+                for i in range(num_binary_rules):
+                    A, B, C = binary_rules[i][0], binary_rules[i][1], binary_rules[i][2]  # A -> B C
+                    rule_prob = binary_prob[i]
+                    prob = score[B][begin][split] + score[C][split][end] + log(rule_prob)
+                    # accumulate sums (in log domain)
+                    if score[A][begin][end] == -INFINITY:
+                        score[A][begin][end] = prob
+                    else:
+                        score[A][begin][end] = log_add_exp(prob, score[A][begin][end])
+
+    inside = -INFINITY
+    for i in range(num_nonterminals):
+        if score[i][0][sent_len] > -INFINITY:
+            if inside == -INFINITY:
+                inside = score[i][0][sent_len]
+            else:
+                inside = log_add_exp(inside, score[i][0][sent_len])
+
+    return inside
+
+
+cdef float log_add_exp(float a, float b):
+    """Compute log(exp(a) + exp(b)) in a numerically stable way."""
+    if a - b < b - a:  # choose the smaller exponent
+        return b + log(1 + exp(a - b))
+    else:
+        return a + log(1 + exp(b - a))
--- main.py
+++ main.py
@@ -22,17 +22,22 @@ def main(args):
     )
 
     if args.infile:
+
         print(f'Predicting trees for tokens in `{args.infile}` to file `{args.outfile}`...')
+
         if args.parallel:
             trees = predict_from_file_parallel(
                 parser, args.infile, args.num_lines, args.tokenize)
         else:
             trees = predict_from_file(
                 parser, args.infile, args.num_lines, args.tokenize)
+
         with open(args.outfile, 'w') as fout:
             print('\n'.join(trees), file=fout)
+
         if args.show:
             show(args.outfile)
+
         print('Evaluating bracket score...')
         if args.goldfile:
             try:
@@ -40,24 +45,30 @@ def main(args):
                 if args.show:
                     show(args.result)
             except:
-                exit('Could note evaluate trees. Maybe you did not parse the entire file?')
+                exit('Could not evaluate trees. Maybe you did not parse the entire file?')
+
         print('Finished.')
 
     elif args.treefile:
         num_trees = 10 if args.num_lines == None else args.num_lines
-        fscores = []
+
         parses = predict_from_trees(parser, args.treefile)
+
+        fscores = []
         for i in range(num_trees):
             gold, pred, prec, rec, fscore = next(parses)
-            print('*'*79)
+            fscores.append(fscore)
+
             print(f'Tree {i}, f1={fscore:.3f}.')
+            print()
             print('Gold:')
             gold.pretty_print()
+            print()
             print('Pred:')
             pred.pretty_print()
             print()
-            fscores.append(fscore)
-        print('*'*79)
+
+        print()
         print('All F1 =', ' '.join([f'{fscore:.3f}' for fscore in fscores]))
         print('Avg F1 = ', sum(fscores) / len(fscores))
 
@@ -67,17 +78,20 @@ def main(args):
         else:
             # Demo: use a default test-sentence with gold tree.
             sentence, gold = SENT.split(), GOLD
+
         print('Parsing sentence...')
         start = time.time()
         trees = parser(sentence, use_numpy=args.use_numpy)
         elapsed = time.time() - start
         tree, score = trees[0]
         tree = cleanup_tree(tree)
-        print(79*'*')
+
         print('Predicted.')
-        print('logprob: ', score)
+        print()
         tree.pretty_print()
-        print(79*'*')
+        print('Logprob:', score)
+        print()
+
         if not args.sent:
             gold = Tree.fromstring(gold)
             prec, recall, fscore = parser.evalb(gold.pformat(margin=np.inf), tree.pformat(margin=np.inf))
@@ -87,11 +101,17 @@ def main(args):
             print(f'Recall = {recall:.3f}')
             print(f'F1 = {fscore:.3f}')
             print()
+
         print(f'Parse-time: {elapsed:.3f}s.')
+
+        if args.perplexity:
+            perplexity = parser.perplexity(sentence)
+            print('Perplexity:', round(perplexity, 2))
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
+
     argparser.add_argument('--grammar', type=str, default='grammar/train/train.vanilla.grammar')
     argparser.add_argument('--sent', type=str, default='')
     argparser.add_argument('--infile', type=str, default='')
@@ -101,12 +121,14 @@ if __name__ == '__main__':
     argparser.add_argument('--treefile', type=str, default='')
     argparser.add_argument('--evalb_dir', type=str, default='EVALB')
     argparser.add_argument('--use-numpy', action='store_true')
+    argparser.add_argument('--perplexity', action='store_true')
     argparser.add_argument('-n', '--num-lines', type=int, default=-1)
     argparser.add_argument('-q', '--ignore-empty', type=int, default=1000, help='let evalb ignore empty lines')
     argparser.add_argument('-t', '--tokenize', action='store_true')
     argparser.add_argument('-p', '--parallel', action='store_true')
     argparser.add_argument('-s', '--show', action='store_true')
     argparser.add_argument('-b', '--expand-binaries', action='store_true', help='expand binary rules with each possible unary')
+
     args = argparser.parse_args()
 
     main(args)
